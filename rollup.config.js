6ed145f2b883afab90f88e0d688e306a61efeefb
ocavue <ocavue@gmail.com>
2023-05-24 10:35:53 -0400

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import dts from 'rollup-plugin-dts'
 const config = [
   {
     input: './dist/tsc/index.d.ts',
-    output: [{ file: 'dist/my-library.d.ts', format: 'es' }],
+    output: [{ file: 'dist/rollup/index.d.ts', format: 'es' }],
     plugins: [dts()],
   },
 ]
